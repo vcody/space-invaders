508f24f0cda73f1d0c21a6f6300af7148ce693d7
vcody <codyyv@protonmail.com>
2021-08-03 17:54:26 -0700

    updated main.py

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
 import pygame, sys
 
-pygame.init()
-screen_height = 600
-screen_width = 600
-screen = pygame.display.set_mode((screen_width, screen_height))
-clock = pygame.time.Clock()
+class Game:
+    def __init__(self):
+        pass
 
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
+    def run(self):
+        pass
+        # update all sprite groups
+        # draw all sprite groups
 
-    screen.fill((30,30,30))
+if __name__ == '__main__':
+    pygame.init()
+    screen_height = 600
+    screen_width = 600
+    screen = pygame.display.set_mode((screen_width, screen_height))
+    clock = pygame.time.Clock()
+    game = Game()
 
-    pygame.display.flip()
-    clock.tick(60)
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+        screen.fill((30,30,30))
+        game.run()
+
+        pygame.display.flip()
+        clock.tick(60)
