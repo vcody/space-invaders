c4f4d4f6c1431c9f6318e754f5343fa1e9b2a1e0
vcody <codyyv@protonmail.com>
2021-08-04 17:48:31 -0700

    created aliens

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import pygame, sys
 from player import Player
+from alien import Alien
 import obstacle
 
 class Game:
     def __init__(self):
+        # Player
         player_sprite = Player((screen_width/2, screen_height), screen_width, screen_height)
         self.player = pygame.sprite.GroupSingle(player_sprite)
 
+        # Obstacle
         self.shape = obstacle.block_shape
         self.block_size = 6
         self.blocks = pygame.sprite.Group()
@@ -14,6 +17,18 @@ class Game:
         self.obstacle_positions = [ num * (screen_width / self.obstacle_amount) for num in range(self.obstacle_amount) ]
         self.create_obstacles(*self.obstacle_positions, start_x=screen_width/14, start_y=480)
     
+        # Alien
+        self.aliens = pygame.sprite.Group()
+        self.alien_setup(rows=6, cols=8)
+    
+    def alien_setup(self, rows, cols, offset_x=70, offset_y=100, distance_x=60, distance_y=48):
+        for row_index, row in enumerate(range(rows)):
+            for col_index, col in enumerate(range(cols)):
+                x = col_index * distance_x + offset_x
+                y = row_index * distance_y + offset_y
+                alien_sprite = Alien('red', x, y)
+                self.aliens.add(alien_sprite)
+
     def create_obstacle(self, start_x, start_y, offset_x):
         for row_index, row in enumerate(self.shape):
             for col_index, col in enumerate(row):
@@ -34,6 +49,7 @@ class Game:
         self.player.draw(screen)
 
         self.blocks.draw(screen)
+        self.aliens.draw(screen)
 
 if __name__ == '__main__':
     pygame.init()
